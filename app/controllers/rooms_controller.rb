a63bf521c2e2e3089593c0cba9b68d4c46e8f4a0
mike-koala-bear <55577@pm.me>
2022-12-04 13:45:25 +0000

    Added most recent message in chatrooms list and more fixes

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,7 +5,7 @@ class RoomsController < ApplicationController
 
   def index
     @room = Room.new
-    @joined_rooms = current_user.joined_rooms
+    @joined_rooms = current_user.joined_rooms.order('last_message_at DESC')
     @rooms = search_rooms
 
     @users = User.all_except(current_user)
@@ -17,7 +17,7 @@ class RoomsController < ApplicationController
 
     @room = Room.new
     @rooms = search_rooms
-    @joined_rooms = current_user.joined_rooms
+    @joined_rooms = current_user.joined_rooms.order('last_message_at DESC')
 
     @message = Message.new
 
