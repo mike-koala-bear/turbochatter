75e3da6811ee2363ba6264453a9380e3e658ac35
mike-koala-bear <55577@pm.me>
2022-12-05 08:27:58 +0000

    Fixed images and more fixes :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,24 @@ class User < ApplicationRecord
   after_create_commit { broadcast_append_to 'users' }
   after_update_commit { broadcast_update }
   has_many :messages
+  has_one_attached :avatar
   has_many :joinables, dependent: :destroy
   has_many :joined_rooms, through: :joinables, source: :room
 
   enum role: %i[user admin]
   enum status: %i[offline away online]
 
-  has_one_attached :avatar do |attachable|
-    attachable.variant :avatar_thumbnail, resize_to_limit: [150, 150]
-    attachable.variant :chat_avatar, resize_to_limit: [50, 50]
-  end
-
   after_commit :add_default_avatar, on: %i[create update]
 
   after_initialize :set_default_role, if: :new_record?
+
+  def avatar_thumbnail
+    avatar.variant(resize_to_limit: [150, 150]).processed
+  end
+
+  def chat_avatar
+    avatar.variant(resize_to_limit: [50, 50]).processed
+  end
 
   def broadcast_update
     broadcast_replace_to 'user_status', partial: 'users/status', user: self
