335224dd79b0366edc03474a44c1464de1dc4097
mike-koala-bear <55577@pm.me>
2022-12-09 08:02:59 +0000

    Add recent message activity stream and more improvements 😀

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,18 @@
 class PagesController < ApplicationController
   after_action :set_status
+  before_action :recent_messages, only: [:home]
   def home
+    recent_messages
   end
 
   private
   def set_status
     current_user&.update!(status: User.statuses[:offline])
+  end
+
+  def recent_messages
+    public_rooms = Room.public_rooms
+
+    @messages = Message.where(room: public_rooms).order(created_at: :desc).limit(15)
   end
 end
